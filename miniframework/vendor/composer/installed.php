81636c8978ba36d089c8631173fbd386f238e5fa
Eugues <euguesgithub@gmail.com>
2022-09-09 19:37:31 -0400

    Base MVC montada
    Base MVC pronta para projetos futuros

---
+++ miniframework/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'miniframework/package-name',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '0272356e5a13b6f23ba7f33dfbdf0d1cebde9482',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'miniframework/package-name' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '0272356e5a13b6f23ba7f33dfbdf0d1cebde9482',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
